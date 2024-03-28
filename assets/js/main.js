f1b2baf3755a3f4b99e5bf37516f258532e6b2eb
Leonardo Lodi <115150826+Leonardolodi@users.noreply.github.com>
2024-03-28 10:43:06 +0100

    transition speed

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,10 +9,10 @@ var settings = {
 
 		// Transition speed (in ms)
 		// For timing purposes only. It *must* match the transition speed of "#banner > article".
-			speed: 4000,
+			speed: 3000,
 
 		// Transition delay (in ms)
-			delay: 5000,
+			delay: 6000,
 
 		// Parallax intensity (between 0 and 1; higher = more intense, lower = less intense; 0 = off)
 			parallax: 0.25
